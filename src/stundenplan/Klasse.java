5f0c64cd799d53e9396363808d0eee66128e25bd
hannah <>
2019-11-08 11:56:33 +0100

    test commit

---
+++ src/stundenplan/Klasse.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stundenplan;
+
+/**
+ *
+ * @author merkerfabian
+ */
+public class Klasse {
+    private int id;
+    private String name;
+}
