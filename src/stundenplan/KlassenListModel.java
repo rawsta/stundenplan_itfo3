6932898b861df05e780d75a9543ff1aa5ce8715e
Sebastian Fiele <rawsta@rawsta.de>
2019-11-11 13:08:06 +0100

    KlassenListModel hinzugefügt

---
+++ src/stundenplan/KlassenListModel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stundenplan;
+
+import javax.swing.AbstractListModel;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author rawsta
+ */
+public class KlassenListModel extends AbstractListModel {
+    
+    /* Daten für das ListModel */
+    private List<Klasse> klassen = new ArrayList<>();
+
+    /** 
+     * constructor for game table
+     * @param klassen 
+     */
+    public KlassenListModel(List<Klasse> klassen) {
+        this.klassen = klassen;
+    }
+    
+    
+    @Override
+    public int getSize() {
+        return klassen.size();
+    }
+
+
+    @Override
+    public Object getElementAt(int index) {
+        
+        Klasse tempKlasse = klassen.get(index);
+        
+        return tempKlasse.getName();
+    }
+}
