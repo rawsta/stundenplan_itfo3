dd85d1a788e17cf3fb0222293bcf249abdd803c8
Sebastian Fiele <rawsta@rawsta.de>
2019-11-18 20:08:34 +0100

    Getter & Setter hinzugefügt

--- src/stundenplan/Fach.java
+++ src/stundenplan/Fach.java
@@ -21,4 +21,29 @@ public class Fach {
         
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKuerzel() {
+        return kuerzel;
+    }
+
+    public void setKuerzel(String kuerzel) {
+        this.kuerzel = kuerzel;
+    }
+
+    
 }
